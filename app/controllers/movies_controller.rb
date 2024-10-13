7682ceb5e6545c26122c88b2b4e98b0039e4b72d
jerome <jeromeabelida19@gmail.com>
2024-10-13 22:37:39 +0800

    added: create in movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,27 @@
 class MoviesController < ApplicationController
+
   def index
     @movies = Movie.all
+  end
+
+  def new
+    @movie  = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = 'Succesfully added'
+      rendirect_to movies_path
+    else
+      flash[:notice] = 'failed to add'
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :blurb, :date_released, :country_of_origin, :showing_start, :showing_end, :image)
   end
 end
