24e8fcee32fd943c861cc5a110ba94ed4909da2e
jerome <jeromeabelida19@gmail.com>
2024-10-13 21:11:23 +0800

    added: gemfile carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_13_114000) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_13_130103) do
   create_table "movies", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "blurb"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_13_114000) do
     t.datetime "showing_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
