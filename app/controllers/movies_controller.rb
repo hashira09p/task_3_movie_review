a3a518e5c1e7e877152ad87d7318dc43b9ad81c1
jerome <jeromeabelida19@gmail.com>
2024-10-14 00:21:03 +0800

    added: update method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,19 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
     if @movie.save
       flash[:notice] = 'Succesfully added'
-      rendirect_to movies_path
+      redirect_to movies_path
+    else
+      flash[:notice] = 'failed to add'
+      render :new
+    end
+  end
+
+  def edit;end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:notice] = 'Succesfully added'
+      redirect_to movies_path
     else
       flash[:notice] = 'failed to add'
       render :new
@@ -20,6 +32,10 @@ class MoviesController < ApplicationController
   end
 
   private
+
+  def set_movie
+    @movie = Movie.find(params [:id])
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :blurb, :date_released, :country_of_origin, :showing_start, :showing_end, :image)
