853e188ea916a57fe6146cef8aa70e9d1846d649
jerome <jeromeabelida19@gmail.com>
2024-10-13 19:58:07 +0800

    added: gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_13_100716) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_13_114000) do
   create_table "movies", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "blurb"
@@ -20,6 +20,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_13_100716) do
     t.datetime "showing_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
