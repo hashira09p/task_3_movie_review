b066b39b058a1d93e4b0dad27f0fa1b39fb4c3d7
Je Rome <jeromeabelida19@gmail.com>
2024-10-14 17:42:50 +0800

    added: reviews and delete the Movie data seed.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,45 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+  before_action :set_review, only: [:edit, :update, :destroy]
+  def index
+    @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.build
+  end
+
+  def create
+    @review = @movie.reviews.build(review_params)
+    if @review.save
+      flash[:notice] = 'Review created successfully'
+      redirect_to movie_reviews_path(@movie)
+    else
+      flash.now[:alert] = 'Failed to create review'
+      render :new
+    end
+  end
+
+  def edit;end
+
+  def update
+    if @review.update(review_params)
+      flash[:notice] = 'Model updated successfully'
+      redirect_to movie_reviews_path(@movie)
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find params[:movie_id]
+  end
+
+  def set_review
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,33 @@
-Movie.create([
-  {
-    title: 'Inception',
-    blurb: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.',
-    date_released: '2010-07-16',
-    country_of_origin: 'USA',
-    showing_start: '2023-10-01 10:00:00',
-    showing_end: '2023-10-31 23:59:59'
-  },
-  {
-    title: 'Parasite',
-    blurb: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.',
-    date_released: '2019-05-30',
-    country_of_origin: 'South Korea',
-    showing_start: '2023-11-01 10:00:00',
-    showing_end: '2023-11-30 23:59:59'
-  },
-  {
-    title: 'The Matrix',
-    blurb: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
-    date_released: '1999-03-31',
-    country_of_origin: 'USA',
-    showing_start: '2023-12-01 10:00:00',
-    showing_end: '2023-12-31 23:59:59'
-  },
-  {
-    title: 'Amélie',
-    blurb: 'Amélie is a young woman who decides to change the lives of those around her for the better, while grappling with her own isolation.',
-    date_released: '2001-04-25',
-    country_of_origin: 'France',
-    showing_start: '2024-01-01 10:00:00',
-    showing_end: '2024-01-31 23:59:59'
-  }
-])
+
+Genre.create([
+               { name: 'Fiction' },
+               { name: 'Non-Fiction' },
+               { name: 'Science Fiction' },
+               { name: 'Fantasy' },
+               { name: 'Mystery' },
+               { name: 'Biography' },
+               { name: 'Romance' },
+               { name: 'Thriller' },
+               { name: 'Historical' },
+               { name: 'Self-Help' },
+               { name: 'Adventure' },
+               { name: 'Horror' },
+               { name: 'Young Adult (YA)' },
+               { name: 'Children\'s Literature' },
+               { name: 'Dystopian' },
+               { name: 'Magical Realism' },
+               { name: 'Graphic Novel' },
+               { name: 'Poetry' },
+               { name: 'Crime' },
+               { name: 'Anthology/Short Stories' },
+               { name: 'Classic' },
+               { name: 'LitRPG' },
+               { name: 'Chick Lit' },
+               { name: 'Western' },
+               { name: 'Political Thriller' },
+               { name: 'Cyberpunk' },
+               { name: 'Historical Fiction' },
+               { name: 'Urban Fantasy' },
+               { name: 'Family Saga' },
+               { name: 'Metafiction' }
+             ]);
